e16aa8833ad8a5b02066146afd7c0aaf736e0d5c
GreatZhuKai <1781308349@qq.com>
2018-01-13 19:37:33 +0800

    element-ui源码

--- router-post/src/App.vue
+++ router-post/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <el-badge class="item" :value="1" :max="9">
+      <el-button size="medium" @click="addComment" nativeType="submit">评论</el-button>
+    </el-badge>
+    <el-badge class="item" :value="1" :max="9" isDot>
+      <el-button :disabled="saveDisabled" @click="doSave" size="small" type="primary">发布</el-button>
+    </el-badge>
+    <el-badge class="item" :value="10" :max="9">
+      <el-button size="mini" type="text">前往</el-button>
+    </el-badge>
   </div>
 </template>
 
 <script>
+import ElBadge from '@/components/ElBadge'
+import ElButton from '@/components/ElButton'
 import HelloWorld from './components/HelloWorld'
 
 export default {
+  data() {
+    return {
+      saveDisabled:false
+    }
+  },
   name: 'app',
+  mounted() {
+    setTimeout(() => {this.saveDisabled = true},5000)
+  },
+  methods:{
+    addComment() {
+      console.log("添加评论")
+    },
+    doSave() {
+      console.log('发布')
+    }
+  },
   components: {
-    HelloWorld
+    ElButton,
+    ElBadge
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ router-post/src/components/ElBadge.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="el-badge">
+      <slot></slot>
+      <transition name="el-zoom-in-center">
+          <sup v-text="content" v-show="!hidden&&(content||isDot)" class="el-badge__content"
+           :class="{'is-fixed':true, 'is-dot':isDot}"></sup>
+      </transition>
+  </div>
+</template>
+<script>
+export default {
+  name:'ElBadge',
+  props:{
+      value:{},
+      max:Number,
+      isDot:Boolean,
+      hidden:Boolean
+  },
+  // 计算属性
+  computed:{
+      content() {
+          if(this.isDot) return;
+          const value = this.value;
+          const max = this.max;
+
+          if(typeof value === 'number' && typeof max === 'number') {
+              return max < value ? `${max}+` : value;
+          }
+      }
+  }
+}
+</script>
+<style>
+
+</style>
